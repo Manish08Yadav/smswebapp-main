d7e4220d51ca769b51c40e74937d934269124ea4
Manish <manish08.yadav06@gmail.com>
2026-09-23 09:44:48 +0530

    added global.json

--- smswebapp/obj/Debug/net8.0/smswebapp.AssemblyInfo.cs
+++ smswebapp/obj/Debug/net8.0/smswebapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("smswebapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b473aba7674919486b331fe817fb0517d505ec5")]
 [assembly: System.Reflection.AssemblyProductAttribute("smswebapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("smswebapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
